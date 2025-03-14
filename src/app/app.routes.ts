ba23d54717cce187808aa10f95347053d8c3d544
Fran-Bar03 <papihachi_vlk@outlook.com>
2025-03-14 00:27:43 -0700

    1 avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CrearCuentaComponent } from './Components/CrearCuenta/crear-cuenta/crear-cuenta.component';
+import { PaginaPrincipalComponent } from './Components/PaginaPrincipal/pagina-principal/pagina-principal.component';
+import { RecuperarContrasenaComponent } from './Components/RecuperarContrasena/recuperar-contrasena/recuperar-contrasena.component';
+import { UsuariosComponent } from './Components/Usuarios/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+{path : 'Crear-Cuenta', component: CrearCuentaComponent},
+{path : 'Pagina-Principal', component: PaginaPrincipalComponent},
+{path: 'Recuperar-Contrasena', component : RecuperarContrasenaComponent},
+{path: 'Usuarios', component: UsuariosComponent},
+{ path: '', redirectTo: '/pagina-principal', pathMatch: 'full' }
+];
