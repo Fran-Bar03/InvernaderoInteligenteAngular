ffcc0ca4ae58f4ec345b67381d89fe5f347871fa
xCorick <corickgonzalez123@gmail.com>
2025-03-19 14:56:25 -0700

    4 avance

--- src/app/Components/estadisticas/estadisticas.component.ts
+++ src/app/Components/estadisticas/estadisticas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
 
 
 @Component({
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
   templateUrl: './estadisticas.component.html',
   styleUrl: './estadisticas.component.css'
 })
-export class EstadisticasComponent {
+export class EstadisticasComponent implements OnInit {
+  public chart: any;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    this.chart = new Chart('grafico', {
+      type: 'line', // Tipo de gráfico (línea)
+      data: {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'], // Etiquetas del eje X
+        datasets: [{
+          label: 'Temperatura', // Nombre de la serie de datos
+          data: [22, 25, 28, 24, 20], // Datos de la serie (temperaturas)
+          borderColor: 'rgba(75, 192, 192, 1)', // Color de la línea
+          borderWidth: 2, // Ancho de la línea
+          fill: false // No rellenar el área debajo de la línea
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: { 
+
+          
+        }
+      }
+    });
+  }
 }
+
+function createChart() {
+  throw new Error('Function not implemented.');
+}
+
